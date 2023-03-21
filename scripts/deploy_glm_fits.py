362f59d7fdb9620b9e16e4367960554dff1a8c07
iryna <irynayavorska@gmail.com>
2023-03-20 20:56:38 -0700

    testing out loading expt table with cache

--- scripts/deploy_glm_fits.py
+++ scripts/deploy_glm_fits.py
@@ -11,7 +11,7 @@ from simple_slurm import Slurm
 import visual_behavior_glm.database as db
 from brain_observatory_analysis.ophys.experiment_loading import start_lamf_analysis
 from brain_observatory_analysis.dev import data_selection_tools as dst
-# from allensdk.brain_observatory.behavior.behavior_project_cache import VisualBehaviorOphysProjectCache
+from allensdk.brain_observatory.behavior.behavior_project_cache import VisualBehaviorOphysProjectCache
 # from mindscope_qc.data_access import behavior_ophys_experiment_dev as BOE_dev
 
 parser = argparse.ArgumentParser(description='deploy glm fits to cluster')
@@ -156,8 +156,8 @@ if __name__ == "__main__":
     else:
        
         # cache = VisualBehaviorOphysProjectCache.from_lims()
-        # experiments_table = cache.get_ophys_experiment_table()
-        experiments_table = start_lamf_analysis()
+        experiments_table = cache.get_ophys_experiment_table()
+        # experiments_table = start_lamf_analysis()
         # experiments_table = dst.limit_to_last_familiar_second_novel(experiments_table)
         run_params = glm_params.load_run_json(args.version)
         projects = gft.define_project_codes()
@@ -165,7 +165,7 @@ if __name__ == "__main__":
         experience_levels = gft.define_experience_levels()
         experiments_table = experiments_table[(experiments_table.cre_line.isin(cre_lines)) & 
                                               (experiments_table.project_code.isin(projects)) &
-                                               (experiments_table.experience_levels.isin(experience_levels))]
+                                              (experiments_table.experience_levels.isin(experience_levels))]
         # if run_params['include_4x2_data']:
         #     print('including 4x2 data')
         #     experiments_table = experiments_table[(experiments_table.reporter_line!="Ai94(TITL-GCaMP6s)")].reset_index()      
