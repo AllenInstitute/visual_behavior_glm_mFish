159c4bd3ec0f8bb94a942068ab455c4846465646
iryna <irynayavorska@gmail.com>
2023-04-06 15:31:40 -0700

    new version

--- scripts/deploy_glm_fits.py
+++ scripts/deploy_glm_fits.py
@@ -16,7 +16,7 @@ from brain_observatory_analysis.dev import data_selection_tools as dst
 
 parser = argparse.ArgumentParser(description='deploy glm fits to cluster')
 parser.add_argument('--env-path', type=str, default='/home/iryna.yavorska/anaconda3/envs/mfish_glm/', metavar='path to conda environment to use')
-parser.add_argument('--version', type=str, default='testing_06_events', metavar='glm version')
+parser.add_argument('--version', type=str, default='testing_07_events', metavar='glm version')
 parser.add_argument(
     '--src-path', 
     type=str, 
